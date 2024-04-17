ed2007d797d488d8b98e84856bb0631b647ab9a5
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2024-04-17 14:28:17 +0800

    feat: setup zustand store

---
+++ src/store/useCartStore.ts
@@ -0,0 +1,56 @@
+import { create } from 'zustand';
+import { CartItem } from '~/types';
+
+interface CartState {
+  cartItems: CartItem[];
+  getItemCount: () => number;
+  getTotalPrice: () => number;
+  addItem: (item: CartItem) => void;
+  removeItem: (id: string) => void;
+  updateQuantity: (item: CartItem) => void;
+  clearCart: () => void;
+}
+
+const useCartStore = create<CartState>()((set, get) => ({
+  cartItems: [],
+
+  getTotalPrice: () =>
+    get().cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0),
+  getItemCount: () =>
+    get().cartItems.reduce((acc, item) => acc + item.quantity, 0),
+
+  addItem: newItem =>
+    set(state => {
+      const sameIdx = state.cartItems.findIndex(item => item.id === newItem.id);
+
+      if (sameIdx !== -1) {
+        const newCartItems = [...state.cartItems];
+        newCartItems[sameIdx].quantity += newItem.quantity;
+        return {
+          cartItems: newCartItems,
+        };
+      }
+      return {
+        cartItems: [...state.cartItems, newItem],
+      };
+    }),
+
+  removeItem: id =>
+    set(state => ({
+      cartItems: state.cartItems.filter(item => item.id !== id),
+    })),
+
+  updateQuantity: updatedItem =>
+    set(state => ({
+      cartItems: state.cartItems.map(item =>
+        item.id === updatedItem.id ? updatedItem : item
+      ),
+    })),
+
+  clearCart: () =>
+    set(() => ({
+      cartItems: [],
+    })),
+}));
+
+export { useCartStore };
